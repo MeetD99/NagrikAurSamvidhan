72033a76dc3a916838395d22bd86dd75db3e0f55
MeetD99 <meetdholakia2074@gmail.com>
2024-09-05 17:38:09 +0530

    modal

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { persistor, store } from './redux/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
